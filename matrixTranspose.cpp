15fe6ae832852e86a0268e66bd73acbe9cf90975
ProgrammerShri <shrikantdewangan3@gmail.com>
2021-06-08 23:15:06 +0530

    Array and Character Array Done..

--- matrixTranspose.cpp
+++ matrixTranspose.cpp
@@ -7,7 +7,6 @@ int main()
     int n, m;
     cin >> n >> m;
     int arr[n][m];
-    // int temp;
     int transpose[n][m];
 
     for (int i = 0; i < n; i++)
@@ -17,24 +16,33 @@ int main()
             cin >> arr[i][j];
         }
     }
+
+    //****************** Logic 1 for Transpose ************************
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-
-            // temp = arr[i][j];
-            // arr[i][j] = arr[j][i];
-            // arr[j][i] = temp;
             transpose[j][i] = arr[i][j];
         }
     }
 
-    for (int i = 0; i < n; i++)
+    //******************* Logic 2 for Transpose ***********************
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 1; j < m; j++)
+    //     {
+    //         int temp = arr[i][j];
+    //         arr[i][j] = arr[j][i];
+    //         arr[j][i] = temp;
+    //     }
+    // }
+
+     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            cout << transpose[i][j] << " ";
-            // cout << arr[i][j] << " ";
+            cout << transpose[i][j] << " "; //print for logic 1
+            // cout << arr[i][j] << " ";       //print for logic 2
         }
         cout << endl;
     }
