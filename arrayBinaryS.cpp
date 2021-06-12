c8692d7154fb294f912b97eeac5cf97d749ba6ff
ProgrammerShri <shrikantdewangan3@gmail.com>
2021-06-12 18:44:56 +0530

    for loop removed from binarySearch fxn

--- arrayBinaryS.cpp
+++ arrayBinaryS.cpp
@@ -4,27 +4,25 @@ using namespace std;
 int binarySearch(int arr[], int n, int key)
 {
 
-    for (int i = 0; i <= n; i++)
+    int s = 0;
+    int e = n;
+    while (s <= e)
     {
-        int s = 0;
-        int e = n;
-        while (s <= e)
+        int mid = (s + e) / 2;
+        if (arr[mid] == key)
         {
-            int mid = (s + e) / 2;
-            if (arr[mid] == key)
-            {
-                return mid;
-            }
-            else if (arr[mid] > key)
-            {
-                e = mid - 1;
-            }
-            else
-            {
-                s = mid + 1;
-            }
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
         }
     }
+
     return -1;
 }
 
@@ -36,7 +34,7 @@ int main()
     cin >> n;
     int arr[n];
 
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
