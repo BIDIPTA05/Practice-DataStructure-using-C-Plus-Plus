a2eb39a72528a37357858ad5778d5469db90e644
programmershri <shrikantdewangan3@gmail.com>
2021-10-30 23:00:29 +0530

    Tree...

--- 001test.cpp
+++ 001test.cpp
@@ -1,20 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool test(string s, string t)
-{
-    sort(s.begin(), s.end());
-    sort(t.begin(), t.end());
-
-    if (s == t)
-        return true;
-    return false;
-}
-
 int main()
 {
-    string s = "rat", t = "car";
-    cout << test(s, t);
+    int n;
+    cin >> n;
+    int arr[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            v.push_back(arr[i][j]);
+        }
+    }
+    int k;
+    cin >> k;
+
+    sort(v.begin(), v.end());
+    cout << endl;
+
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+
+        if (i == k - 1)
+        {
+            cout << v[i];
+        }
+    }
 
     return 0;
 }
