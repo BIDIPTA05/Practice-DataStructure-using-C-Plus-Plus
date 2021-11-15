2a365a3ecf918f33763491db6cf8da82ccf1cab3
programmershri <shrikantdewangan3@gmail.com>
2021-11-15 20:54:23 +0530

    Tree...

--- 001test.cpp
+++ 001test.cpp
@@ -1,48 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int catalan(int n)
+{
+    if (n <= 1)
+        return 1;
+    int res = 0;
+
+    for (int i = 0; i <= n - 1; i++)
+    {
+        res += catalan(i) * catalan(n - i - 1);
+    }
+
+    return res;
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n][n];
-
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 10; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> arr[i][j];
-        }
+        cout << catalan(i) << " ";
     }
-
-    vector<int> v;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            v.push_back(arr[i][j]);
-        }
-    }
-    int k;
-    cin >> k;
-
-    sort(v.begin(), v.end());
-    cout << endl;
-
-    for (auto i : v)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    for (int i = 0; i < v.size(); i++)
-    {
-
-        if (i == k - 1)
-        {
-            cout << v[i];
-        }
-    }
-
     return 0;
 }
